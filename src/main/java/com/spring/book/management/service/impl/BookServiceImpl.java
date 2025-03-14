1efbd65431c88aefc2f90a90bbddc5790af0a94b
Oleksii <obabych1@stu.vistula.edu.pl>
2025-03-14 13:29:59 +0100

    adding DTOs, mappers, endpoints and implementation of methods in service and repository

--- src/main/java/com/spring/book/management/service/impl/BookServiceImpl.java
+++ src/main/java/com/spring/book/management/service/impl/BookServiceImpl.java
@@ -1,17 +1,51 @@
 package com.spring.book.management.service.impl;
 
+import com.spring.book.management.dao.BookRepository;
+import com.spring.book.management.dto.BookDto;
+import com.spring.book.management.dto.CreateBookRequestDto;
+import com.spring.book.management.exception.EntityNotFoundException;
+import com.spring.book.management.mapper.BookMapper;
 import com.spring.book.management.model.Book;
 import com.spring.book.management.service.BookService;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BookServiceImpl implements BookService {
-    @Override
-    public Book save(Book book) {
-        return null;
+
+    private final BookRepository bookRepository;
+    private final BookMapper bookMapper;
+
+    public BookServiceImpl(BookRepository bookRepository, BookMapper bookMapper) {
+        this.bookRepository = bookRepository;
+        this.bookMapper = bookMapper;
     }
 
     @Override
-    public List<Book> findAll() {
-        return null;
+    public BookDto save(CreateBookRequestDto createBookRequestDto) {
+        Book book = bookMapper.toModel(createBookRequestDto);
+        Book savedBook = bookRepository.save(book);
+
+        return bookMapper.toDto(savedBook);
+    }
+
+    @Override
+    public BookDto findById(Long id) {
+        Optional<Book> bookOptional = bookRepository.findById(id);
+        if (bookOptional.isEmpty()) {
+            throw new EntityNotFoundException("Book with ID " + id + " not found");
+        }
+        return bookOptional.map(bookMapper::toDto).orElse(null);
+    }
+
+    @Override
+    public List<BookDto> findAll() {
+        List<Book> books = bookRepository.findAll();
+
+        return books.stream()
+                .map(bookMapper::toDto)
+                .collect(Collectors.toList());
     }
 }
